f484cc1821cb3d4df0fa324cc28d13402713148c
davis <zanxiaofeng@163.com>
2022-12-07 10:41:21 +0800

    Development

--- src/main/java/zxf/example/release/jar/ReleaseJar.java
+++ src/main/java/zxf/example/release/jar/ReleaseJar.java
@@ -2,6 +2,6 @@ package zxf.example.release.jar;
 
 public class ReleaseJar {
     public static void main() {
-        System.out.println("release-jar: v1.0.0");
+        System.out.println("release-jar: v1.0.1");
     }
 }
